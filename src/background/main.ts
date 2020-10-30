80f8fc8d001210b73316d9c752bcdef8bf04a4a3
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-30 18:28:38 +0100

    Send an array of points for edges now

--- src/background/main.ts
+++ src/background/main.ts
@@ -9,6 +9,10 @@ const createBG = () => new p5((p: p5) => {
     p.myScale = 1
     p.translationX = 0 // in pixels
     p.translationY = 0 // in pixels
+    p.nodesX1 = [0.1, 0.3]
+    p.nodesY1 = [0.2, 0.4]
+    p.nodesX2 = [0.3, 0.1]
+    p.nodesY2 = [0.4, 0.6]
     
     p.setup = function() {
         p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL)
@@ -22,10 +26,10 @@ const createBG = () => new p5((p: p5) => {
         p.noStroke()
         p.shader(myShader)
         myShader.setUniform("uAspectRatio", p.width / p.height)
-        myShader.setUniform("x1", 0)
-        myShader.setUniform("y1", 0)
-        myShader.setUniform("x2", 0.5 * p.width / p.height)
-        myShader.setUniform("y2", 0.5)
+        myShader.setUniform("x1", p.nodesX1)
+        myShader.setUniform("y1", p.nodesY1)
+        myShader.setUniform("x2", p.nodesX2)
+        myShader.setUniform("y2", p.nodesY2)
         myShader.setUniform("scale", p.myScale)
         myShader.setUniform("translation", [p.translationX / p.height, p.translationY / p.height])
         p.rect(0, 0, p.width, p.height)
--- src/background/myShader.frag
+++ src/background/myShader.frag
@@ -4,10 +4,10 @@ varying vec2 vTexCoord;
 uniform float uAspectRatio;
 uniform float scale;
 uniform vec2 translation;
-uniform float x1;
-uniform float y1;
-uniform float x2;
-uniform float y2;
+uniform float x1[2];
+uniform float y1[2];
+uniform float x2[2];
+uniform float y2[2];
 
 float sdSegment(vec2 p, vec2 a, vec2 b, float r)
 {
@@ -22,7 +22,10 @@ void main() {
     uv -= translation;
     uv /= scale;
 
-    float d = sdSegment(uv, vec2(x1, y1), vec2(x2, y2), 0.001);
+    float d = 100000.;
+    for (int i = 0; i < 2; ++i) {
+        d = min(d, sdSegment(uv, vec2(x1[i], y1[i]), vec2(x2[i], y2[i]), 0.001));
+    }
     float t = smoothstep(max(d, 0.), 0., 0.002);
     vec3 col = mix(vec3(1.), vec3(0.1), t);
     
