144a4c1dcf499c337d21ecac90c9c1824a1a0432
Jules Fouchy <jules.fouchy@ntymail.com>
2020-11-15 19:24:42 +0100

    Add glow on nodes

--- src/components/ArticleThumbnail.ts
+++ src/components/ArticleThumbnail.ts
@@ -4,6 +4,7 @@ import ArticleDescription from '../articles/ArticleDescription'
 export default (x: number, y: number, article: ArticleDescription, idx: number) => {
     const radius = 50 // in pixels
     const followLink = "window.location.href = 'articles/"+ article.folderName +"/index.html'"
+    const color = '200, 100, 255'
     return h('i', 
         {
             class: `article-thumbnail fas fa-${article.icon}`,
@@ -12,6 +13,10 @@ export default (x: number, y: number, article: ArticleDescription, idx: number) 
                 top:  calc(${y * 100}vh - ${radius}px);
                 width:  ${radius*2}px;
                 height: ${radius*2}px;
+                color: rgb(${color});
+                box-shadow: 0px 0px 10px 3px rgba(${color}, 0.75);
+                -webkit-box-shadow: 0px 0px 10px 3px rgba(${color}, 0.75);
+                -moz-box-shadow: 0px 0px 10px 3px rgba(${color}, 0.75);
             `,
             onmouseover: `window.dispatchEvent(new CustomEvent('articleHovered', {detail: {idx: ${idx}}}))`,
             onclick: followLink,
