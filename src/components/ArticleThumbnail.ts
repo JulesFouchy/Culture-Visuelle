c47d95ea19b2333e79075c6675b460280d77b398
Amandine <akohlmuller@gmail.com>
2020-11-18 14:53:22 +0100

    Change colors

--- src/components/ArticleThumbnail.ts
+++ src/components/ArticleThumbnail.ts
@@ -3,9 +3,9 @@ import { Category , ArticleDescriptionAndPosition } from '../articles/ArticleDes
 
 export default (article : ArticleDescriptionAndPosition, idx: number, radius: number = 50) => {
     const followLink = "window.location.href = 'articles/"+ article.desc.folderName +"/index.html'"
-    const color = article.desc.category === Category.Cultural     ? ' 60, 209,  52' :
-                  article.desc.category === Category.Memorisation ? '200, 100, 255' :
-                  article.desc.category === Category.Technical    ? ' 59, 156, 247'
+    const color = article.desc.category === Category.Cultural     ? ' 255, 184, 0' :
+                  article.desc.category === Category.Memorisation ? '39, 218, 121' :
+                  article.desc.category === Category.Technical    ? ' 110, 51, 206'
                   : '255, 255, 255'
     return h('i', 
         {
@@ -14,12 +14,12 @@ export default (article : ArticleDescriptionAndPosition, idx: number, radius: nu
             style: `
                 left: calc(${article.currentPos.x * 100}vw - ${radius}px);
                 top:  calc(${article.currentPos.y * 100}vh - ${radius}px);
-                width:  ${radius*2}px;
-                height: ${radius*2}px;
+                width:  ${radius*1.75}px;
+                height: ${radius*1.75}px;
                 color: rgb(${color});
-                box-shadow: 0px 0px 10px 3px rgba(${color}, 0.75);
-                -webkit-box-shadow: 0px 0px 10px 3px rgba(${color}, 0.75);
-                -moz-box-shadow: 0px 0px 10px 3px rgba(${color}, 0.75);
+                box-shadow: 0px 0px 10px 3px rgba(${color}, 0.5);
+                -webkit-box-shadow: 0px 0px 10px 3px rgba(${color}, 0.5);
+                -moz-box-shadow: 0px 0px 10px 3px rgba(${color}, 0.5);
             `,
             onmouseover: `window.dispatchEvent(new CustomEvent('articleHovered', {detail: {idx: ${idx}}}))`,
             onclick: followLink,
