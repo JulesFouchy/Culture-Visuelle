8f64b16f90c520083a1ccdac798c5989eb582d28
Manon Sgro <manon-sgro@outlook.fr>
2020-12-01 09:37:17 +0100

    Article Mansion_Sgro

--- src/articles/Mansion_Sgro/description.ts
+++ src/articles/Mansion_Sgro/description.ts
@@ -1,11 +1,11 @@
 import { ArticleDescription, Category } from '../../types/ArticleDescription'
 
 const description: ArticleDescription = {
-    title: "Mémoire des lieux dans le dessin et la peinture",
+    title: "Mémoire, espaces et arts contemporains",
     icon: "paint-brush",
     iconUnicode: "\uf1fc",
     category: Category.Cultural,
-    authors: ["Amélia MANSION", "Manon SGRO"],
+    authors: ["Amélia MANSION", "Manon SGRO'"],
     // Don't touch the folder name
     folderName: "Mansion_Sgro",
 }
