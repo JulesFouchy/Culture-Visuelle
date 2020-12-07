63d3182ac0acab55231f474bd381913f4b5e8968
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-07 13:03:42 +0100

    [Rosenberg_Vallet] Add article

--- src/articles/Rosenberg_Vallet/description.ts
+++ src/articles/Rosenberg_Vallet/description.ts
@@ -2,9 +2,9 @@ import { ArticleDescription, Category } from '../../types/ArticleDescription'
 
 const description: ArticleDescription = {
     title: "Synesthésie et représentation du son en art numérique",
-    icon: "music",
-    iconUnicode: "\uf001",
-    category: Category.Cultural,
+    icon: "eye",
+    iconUnicode: "\uf06e",
+    category: Category.Memorisation,
     authors: ["Maéva ROSENBERG", "Gaëlle VALLET"],
     // Don't touch the folder name
     folderName: "Rosenberg_Vallet",
