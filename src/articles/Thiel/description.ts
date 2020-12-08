2373609b41375ffd8227718c68d9f8de7ff1950d
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2020-12-08 09:53:49 +0100

    [Thiel] fix descriptino import and format

--- src/articles/Thiel/description.ts
+++ src/articles/Thiel/description.ts
@@ -1,8 +1,9 @@
-import { ArticleDescription, Category } from '../ArticleDescription'
+import { ArticleDescription, Category } from '../../types/ArticleDescription'
 
 const description: ArticleDescription = {
-    title: "Culture et mémoire numérique dans la science-fiction"
+    title: "Culture et mémoire numérique dans la science-fiction",
     icon: "database",
+    iconUnicode: "\uf1c0",
     category: Category.Technical,
     authors: ["Pierre THIEL"],
     // Don't touch the folder name
