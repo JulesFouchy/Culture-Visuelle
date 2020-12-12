142de962122ebf0ccd80a5e4c8a53c73cb7ec493
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-12 11:37:19 +0100

    [Vo] add article

---
+++ src/articles/Vo/description.ts
@@ -0,0 +1,13 @@
+import { ArticleDescription, Category } from '../../types/ArticleDescription'
+
+const description: ArticleDescription = {
+    title: "Mémoire des vêtements",
+    icon: "tshirt",
+    iconUnicode: "\uf553",
+    category: Category.Cultural,
+    authors: ["Minh-Tri VO"],
+    // Don't touch the folder name
+    folderName: "Vo",
+}
+
+export default description
