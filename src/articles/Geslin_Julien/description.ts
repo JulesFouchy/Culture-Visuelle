99ee1596031d8db0bd96405d65a9f4c66563875d
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-13 10:07:08 +0100

    [Geslin_Julien] Add description

--- src/articles/Geslin_Julien/description.ts
+++ src/articles/Geslin_Julien/description.ts
@@ -1,10 +1,10 @@
 import { ArticleDescription, Category } from '../../types/ArticleDescription'
 
 const description: ArticleDescription = {
-    title: "Salut les amis",
+    title: "✨ Comment le jeu vidéo influence-t-il la mémoire ? ✨",
     icon: "lemon",
     iconUnicode: "\uf094",
-    category: Category.Cultural,
+    category: Category.Technical,
     authors: ["Thomas GESLIN", "Victor JULIEN"],
     // Don't touch the folder name
     folderName: "Geslin_Julien",
